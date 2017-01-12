ec860119f7e9acd64bc306aa9b336004d6bcf79b
vkobitchuk <akaynt100@tut.by>
2017-01-12 13:15:12 +0300

    added new pages

--- src/assets/js/product-tabs.js
+++ src/assets/js/product-tabs.js
@@ -1,10 +1,12 @@
 (function($){
-    jQuery.fn.productTabs = function(startIndex){
-        var tabs = $(this),
-            tabsContent = tabs.find("[data-tab-content]"),
-            tabsBtns = tabs.find("[data-tab]");
+    jQuery.fn.productTabs = function(activeTabSelector,startIndex){
+
         var createTabs = function(){
-            //var i = 0;
+            var tabs = $(this),
+                tabsContent = tabs.find("[data-tab-content]"),
+                tabsBtns = tabs.find("[data-tab]");
+
+            tabsContent.hide();
             var showTab = function(i){
                 tabsContent.hide();
                 tabsContent.each(function () {
@@ -16,13 +18,40 @@
             };
 
             tabsBtns.click(function(){
-                //tabsBtns.removeClass("tabs__title-active");
-                //$(this).addClass("tabs__title-active");
+                tabsBtns.removeClass(activeTabSelector);
+                $(this).addClass(activeTabSelector);
                 showTab(parseInt($(this).data("tab")));
             });
 
             (startIndex) ? tabsBtns[parseInt(startIndex)].click() : tabsBtns[0].click();
 
+        };
+        return this.each(createTabs);
+    };
+
+
+    jQuery.fn.selectionTabs = function(activeTabSelector,startIndex){
+        var tabs = $(this),
+            tabsContent = tabs.find("[data-tab-content-upper-level]"),
+            tabsBtns = tabs.find("[data-tab-upper-level]");
+        var createTabs = function(){
+            tabsContent.hide();
+            var showTab = function(i){
+                tabsContent.hide();
+                tabsContent.each(function () {
+                    if($(this).data("tabContentUpperLevel") == i){
+                        $(this).show();
+                    }
+                });
+            };
+
+            tabsBtns.click(function(){
+                tabsBtns.removeClass(activeTabSelector);
+                $(this).addClass(activeTabSelector);
+                showTab(parseInt($(this).data("tabUpperLevel")));
+            });
+
+            (startIndex) ? tabsBtns[parseInt(startIndex)].click() : tabsBtns[0].click();
         };
         return this.each(createTabs);
     };
