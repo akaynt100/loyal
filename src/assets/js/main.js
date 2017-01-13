9c9535608cb62401eefbadc1386d2e2768a8f466
sklif <sklif_@mail.ru>
2017-01-13 20:37:19 +0300

    finish index.html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -271,3 +271,14 @@ $(window).resize(function(){
         $('.catalog').unslick();
     }
 });
+
+// ***** remove this *******
+//$('.catalog').slick({
+//    arrows: true,
+//    slidesToShow: 1,
+//    slidesToScroll: 1,
+//    draggable: true,
+//    nextArrow: '<div class="rightCatalogArrow"><img src="assets/images/scroll-arrow-right.png"></div>',
+//    prevArrow: '<div class="leftCatalogArrow"><img src="assets/images/scroll-arrow-left.png"></div>'
+//});
+//**********************************************
